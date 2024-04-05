9ec5f6f23b11bf5723467792791b16269ffcda5c
eonsoo1 <djstn0613@konkuk.ac.kr>
2024-04-05 22:44:24 +0900

    git init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/eonsoo/2024_graduation_project/src
+# Build directory: /home/eonsoo/2024_graduation_project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/eonsoo/2024_graduation_project/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/eonsoo/carla-ros-bridge/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/eonsoo/carla-ros-bridge/catkin_ws/devel;/opt/ros/noetic' != '' else []
