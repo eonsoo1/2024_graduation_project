d7f96db6da9f67772586ee4b0328dfbbc3d66c31
gyeolhanryu <hgru020304@naver.com>
2024-04-08 14:54:04 +0900

    devel and build directories are removed. 2d lidar package has added.

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/songjimin/2024_graduation_project/src
-# Build directory: /home/songjimin/2024_graduation_project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/songjimin/2024_graduation_project/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/songjimin/catkin_ws/devel;/home/songjimin/jimin_ws/devel;/opt/ros/noetic'.split(';') if '/home/songjimin/catkin_ws/devel;/home/songjimin/jimin_ws/devel;/opt/ros/noetic' != '' else []
